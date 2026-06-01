217e3e47177a2479637477a67ad1aa6121e3bd8d
Nas <naselifbal@gmail.com>
2026-06-01 21:21:40 +0300

    Siparis sınıfı oluşturularak burger içeriği, seçilen ekmek/et, ekstra ve içecek property'leri listeler halinde eklendi.

--- HelloHamburger.Entities/Concrete/Siparis.cs
+++ HelloHamburger.Entities/Concrete/Siparis.cs
@@ -6,7 +6,25 @@ using System.Threading.Tasks;
 
 namespace HelloHamburger.Entities.Concrete
 {
-    internal class Siparis
+    public class Siparis
     {
+        public string BurgerAdi { get; set; }
+        public int SatisFiyati { get; set; }
+
+        // Hamburgerin içindeki malzemeleri (Köfte, Turşu vs.) tutacak liste
+        public List<string> Icerik { get; set; }
+
+        // Menüdeki diğer seçimler
+        public string SecilenEt { get; set; }
+        public string SecilenEkmek { get; set; }
+        public string SecilenEkstra { get; set; }
+        public string SecilenIcecek { get; set; }
+
+        // Constructor: Sipariş oluşturulduğunda listeyi hazır hale getirir
+        public Siparis()
+        {
+            Icerik = new List<string>();
+        }
+
     }
 }
