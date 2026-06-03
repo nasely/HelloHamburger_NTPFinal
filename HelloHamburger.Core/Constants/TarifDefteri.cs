16384d16dbd56bd4cf3e7b1b054497582480a12c
Nas <naselifbal@gmail.com>
2026-06-03 22:29:00 +0300

    Burger içerikleri yazıldı, burger tariflerini tutmak ve isme göre hızlıca çağırmak için Dictionary<string, List> yapısı kullanıldı.

--- HelloHamburger.Core/Constants/TarifDefteri.cs
+++ HelloHamburger.Core/Constants/TarifDefteri.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace HelloHamburger.Core.Constants
 {
-    internal class TarifDefteri
+    public class TarifDefteri
     {
+        public static readonly Dictionary<string, List<string>> Tarifler = new Dictionary<string, List<string>>()
+        {
+            {
+                "Klasik Köfte Hamburger", new List<string>()
+                {
+                    "Marul",
+                    "Domates",
+                    "Turşu"
+                }
+            },
+            {
+                "Çıtır Tavuk Burger", new List<string>()
+                {
+                    "Cheddar peyniri",
+                    "Turşu",
+                    "Burger sosu"
+                }
+            },
+            {
+                "Pastırmalı Burger", new List<string>()
+                {
+                    "Cheddar peyniri",
+                    "Marul",
+                    "Domates",
+                    "Turşu",
+                }
+            },
+            {
+                "Vegan Burger", new List<string>()
+                {
+                    "Marul",
+                    "Domates",
+                    "Turşu"
+                }
+            }
+        };
+
     }
 }
