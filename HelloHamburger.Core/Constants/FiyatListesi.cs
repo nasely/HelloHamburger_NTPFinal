53922143f1c76dbcb6d3a70cbac26ed9d8663be0
Nas <naselifbal@gmail.com>
2026-06-03 22:08:57 +0300

    Satış ve maliyet fiyatları belirlendi oyun içi ekonomi değişkenleri (alış/satış) güvenlik amacıyla 'const' olarak tanımlandı.

--- HelloHamburger.Core/Constants/FiyatListesi.cs
+++ HelloHamburger.Core/Constants/FiyatListesi.cs
@@ -6,7 +6,44 @@ using System.Threading.Tasks;
 
 namespace HelloHamburger.Core.Constants
 {
-    internal class FiyatListesi
+    public class FiyatListesi
     {
+        // Menü Satış Fiyatları 
+        public const int KlasikKofteSatis = 350;
+        public const int CitirTavukSatis = 300;
+        public const int PastirmaliSatis = 350;
+        public const int VeganSatis = 250;
+
+        public const int EkstraPatatesSatis = 50;
+        public const int EkstraSoganSatis = 60;
+        public const int EkstraPeynirSatis = 40;
+
+        public const int IcecekKolaSatis = 80;
+        public const int IcecekAyranSatis = 60;
+        public const int IcecekLimonataSatis = 70;
+
+        // Malzeme Maliyetleri 
+        public const int MaliyetPastirma = 80;
+        public const int MaliyetKofte = 80;
+        public const int MaliyetTavuk = 65;
+        public const int MaliyetVeganKofte = 40;
+        public const int MaliyetCheddar = 15;
+        public const int MaliyetTursu = 10;
+        public const int MaliyetDomates = 10;
+        public const int MaliyetMarul = 10;
+        public const int MaliyetBurgerSos = 10;
+        public const int MaliyetAyran = 20;
+        public const int MaliyetLimonata = 30;
+        public const int MaliyetKola = 3;
+        public const int MaliyetPatates = 30;
+        public const int MaliyetSogan = 40;
+        public const int MaliyetPeynir = 20;
+        public const int MaliyetEkmek = 10;
+
+        // Geliştirme Maliyetleri
+        public const int MaliyetDekorasyon = 1000;
+        public const int MaliyetReklam = 1500;
+
+
     }
 }
